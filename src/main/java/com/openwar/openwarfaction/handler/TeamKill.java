67b7582dd987c70b784ab35c4f8097ca41f7b65b
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-10-20 10:28:35 +0200

    Create TeamKill.java

---
+++ src/main/java/com/openwar/openwarfaction/handler/TeamKill.java
@@ -0,0 +1,37 @@
+package com.openwar.openwarfaction.handler;
+
+import com.openwar.openwarfaction.factions.Faction;
+import com.openwar.openwarfaction.factions.FactionManager;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
+
+public class TeamKill implements Listener {
+
+    private FactionManager fm;
+
+    public TeamKill(FactionManager fm) {
+        this.fm = fm;
+    }
+
+    @EventHandler
+    public void onTeamDamage(EntityDamageByEntityEvent event) {
+        if (event.getEntity() instanceof Player && event.getDamager() instanceof Player) {
+            Player victim = (Player) event.getEntity();
+            Player attacker = (Player) event.getDamager();
+            Faction fac1 = fm.getFactionByPlayer(victim.getUniqueId());
+            Faction fac2 = fm.getFactionByPlayer(attacker.getUniqueId());
+            if (fac1 == fac2) {
+                event.setCancelled(true);
+                attacker.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§8» §fcYou can't attack your mate §4" + victim.getName() + " §8«"));
+            }
+        }
+    }
+}
