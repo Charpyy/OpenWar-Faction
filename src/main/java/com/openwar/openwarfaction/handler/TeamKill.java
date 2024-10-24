32efc5686001bf26da91a331178c764d80cd3de8
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-10-24 09:22:08 +0200

    Update TeamKill.java

--- src/main/java/com/openwar/openwarfaction/handler/TeamKill.java
+++ src/main/java/com/openwar/openwarfaction/handler/TeamKill.java
@@ -23,14 +23,18 @@ public class TeamKill implements Listener {
 
     @EventHandler
     public void onTeamDamage(EntityDamageByEntityEvent event) {
-        if (event.getEntity() instanceof Player && event.getDamager() instanceof Player) {
-            Player victim = (Player) event.getEntity();
-            Player attacker = (Player) event.getDamager();
-            Faction fac1 = fm.getFactionByPlayer(victim.getUniqueId());
-            Faction fac2 = fm.getFactionByPlayer(attacker.getUniqueId());
-            if (fac1 == fac2 && !fm.getTk(fac1)) {
-                event.setCancelled(true);
-                attacker.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§8» §fcYou can't attack your mate §4" + victim.getName() + " §8«"));
+        if (event.getEntity() instanceof Player) {
+            if (event.getDamager() instanceof Player) {
+                Player victim = (Player) event.getEntity();
+                Player attacker = (Player) event.getDamager();
+                Faction fac1 = fm.getFactionByPlayer(victim.getUniqueId());
+                Faction fac2 = fm.getFactionByPlayer(attacker.getUniqueId());
+                if (fac1 == fac2 && !fm.getTk(fac1)) {
+                    event.setCancelled(true);
+                    attacker.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§8» §cYou can't attack your mate §4" + victim.getName() + " §8«"));
+                }
+            } else {
+                System.out.println("DAMAAAGERR = " + event.getDamager());
             }
         }
     }
